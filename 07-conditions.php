289caea629b55e52ddc3eeb2b9ea936d741ff780
Nasir-afridi <nasirwc05@gmail.com>
2025-03-09 01:10:57 +0500

    fixed age range check logic added with the help of if, elseif, else

--- 07-conditions.php
+++ 07-conditions.php
@@ -1,9 +1,27 @@
 <?php
-  $num = 8;
+ 
 
+/*
+  find the even and odd number.
+  $num = 8;
   if($num % 2 == 0) {
     echo "$num is even number.";
   }else{
     echo "$num is odd number.";
+  }
+*/
+  
+  $age = 1088;
+
+  if(($age > 0) and ($age <= 25)) {
+    echo "your age is in range of 0 to 25";
+  }elseif(($age > 25) and ($age <= 50)) {
+    echo "your age is in range of 25 to 50";
+  }elseif(($age > 50) and ($age <= 75)) {
+    echo "your age is in range of 50 to 75";
+  }elseif(($age > 75) and ($age <=  100)) {
+    echo "your age is in range of 75 to 100";
+  }else{
+    echo "Congragulations.....you are over age";
   }
 ?>
