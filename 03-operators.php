82a41d17db0f00a8c2dca5c54a2b61fb0923c5d2
Nasir-afridi <nasirwc05@gmail.com>
2025-03-08 12:09:37 +0500

    Php operators

---
+++ 03-operators.php
@@ -0,0 +1,21 @@
+<?php
+  // $a = 5;
+  // $b = 2;
+  // $result = $a + $b;
+  // $result = $a - $b;
+  // $result = $a / $b;
+  // $result = $a * $b;
+  // $result = $a % $b;
+  // $result = $a ** $b;
+  // echo $result;
+
+  // $a = 4;
+  // $a++; post increment.
+  // ++$a; pre increment.
+  // $a--; post decrement.
+  // --$a; pre decrement.
+  // echo $a;
+
+  $a = true + 3;
+  echo $a;
+?>
