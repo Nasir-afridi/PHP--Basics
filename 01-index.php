7cb5f1f6c3ff1bc5f3e418f1a0c5745cfe00e1a2
Nasir-afridi <nasirwc05@gmail.com>
2025-03-08 12:08:58 +0500

    Basic php code

---
+++ 01-index.php
@@ -0,0 +1,6 @@
+<?php
+// in print we use only one value in both conditions open and paranthesis.
+// we can put many values in open echo. in paranthesis echo only one value is allowed.
+  echo ("nasir khan");
+  echo 'nasir khan', 25, 'wahcantt';
+?>
