366002e080cd9481e3ce4be28292ebd5e2f4d2af
Nasir-afridi <nasirwc05@gmail.com>
2025-03-08 14:03:40 +0500

    two comparison operator added

--- 03-operators.php
+++ 03-operators.php
@@ -1,4 +1,6 @@
 <?php
+
+  /***** Arthematic Operators *****/ 
   // $a = 5;
   // $b = 2;
   // $result = $a + $b;
@@ -16,5 +18,19 @@
   // --$a; pre decrement.
   // $a = true + 3;
   // echo $a;
+
+
+  /***** Comparison Operators *****/
+  // $a = 10;
+  // $b = '10';
+  // ye abhe be true dyga qq kk ye data type nahe balky value chk krta hai kkk same hain ya nahe.
+  // $result = $a == $b;
+  // echo $result;
+
+  //  $a = 10;
+  //  $b = '10';
+  //   ye data type or value dono chk krta hai.
+  //  $result = $a === $b;
+  //  echo $result;
 
   ?>
