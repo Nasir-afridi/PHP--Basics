840b019bd5ee88c81251dee71d6555e63e7e305e
Nasir-afridi <nasirwc05@gmail.com>
2025-03-09 08:10:43 +0500

    for, while, dowhile loop added

---
+++ 09-loops.php
@@ -0,0 +1,56 @@
+<!-- <?php
+
+  
+  // printing even number directly.
+  for($i = 2; $i <= 100; $i=$i+2) {
+    echo "$i <br/>";
+  }
+  
+  // check and then printing the even number.
+  for($i = 2; $i <= 100; $i=$i+2) {
+    if($i % 2 == 0) {
+      echo "$i <br/>";
+    }
+  }
+  ?> -->
+
+
+
+
+
+
+
+
+
+<!-- <?php  
+
+  // directly printed the odd numbers.
+  $a = 1;
+  while($a <= 10) {
+    echo "$a <br />"; 
+    $a = $a + 2;
+  }
+
+  // check and then printing the odd numbers.
+  while($a <= 10) {
+    if($a % 2 == 1) {
+      echo "$a <br />";
+    }
+    $a++;
+  }
+  ?> -->
+
+
+
+
+
+
+
+
+<?php
+  $i = 1;
+  do {
+    echo "$i <br/>";
+    $i++;
+  } while($i <=10)
+?>
