df1ab0edb16286124fc383c61dd540481a0f2d13
Nasir-afridi <nasirwc05@gmail.com>
2025-03-09 14:57:01 +0500

    indexed array added

---
+++ 11-array.php
@@ -0,0 +1,33 @@
+<!-- <?php
+    /*
+    ************************************indexed array***********************************
+    array has two types indexed array assosiative array.
+    index array: for printing the array we use print_r instead of echo.
+    */
+
+
+    $even_numbers = [2, 4, 6, 8, 10];
+    $colors  = array('red', 'blue', 'green', 'black');
+
+
+    // if we want that the green is stored in another variable we do it with the help of index.
+    $x = $colors[2];
+    echo $x;
+
+
+    /* 
+    print_r($even_numbers);
+    echo "<br />";
+    print_r($colors);
+    
+
+    for beauty format we print array like this
+    */
+    echo "<pre>";
+    print_r($colors);
+    echo "</pre>";
+?> -->
+
+<?php
+    
+?>
